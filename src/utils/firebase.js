daecf6a7fe2573010e7a3af8dcac1e5e3748dd7b
Cole Harten <charten@college.harvard.edu>
2023-12-27 01:42:25 -0500

    updated sign in

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,37 +1,36 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import "firebase/compat/firestore";
-import { getFirestore } from "firebase/firestore";
+import 'firebase/compat/firestore';
+import { getFirestore } from 'firebase/firestore';
 import StyledFirebaseAuth from '../components/StyledFirebaseAuth.tsx';
 
-// google sign in 
-import { GoogleAuthProvider } from "firebase/auth";
-
-// ! DO NOT CHANGE THIS FILE.
+// Import additional authentication provider
+import { GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyBtTlAUMih6fxZ73E1vz82YLWD35158a10",
-   authDomain: "hpair-deliv.firebaseapp.com",
-   projectId: "hpair-deliv",
-   storageBucket: "hpair-deliv.appspot.com",
-   messagingSenderId: "974578239414",
-   appId: "1:974578239414:web:ae41aac204347839d92317"
- };
+  apiKey: 'AIzaSyBtTlAUMih6fxZ73E1vz82YLWD35158a10',
+  authDomain: 'hpair-deliv.firebaseapp.com',
+  projectId: 'hpair-deliv',
+  storageBucket: 'hpair-deliv.appspot.com',
+  messagingSenderId: '974578239414',
+  appId: '1:974578239414:web:ae41aac204347839d92317',
+};
 
 // Configure FirebaseUI.
 const uiConfig = {
-   // Popup signin flow rather than redirect flow.
-   signInFlow: 'popup',
-   // We will display Github as auth providers.
-   signInOptions: [
-      GoogleAuthProvider.PROVIDER_ID,
-   ],
-   callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false,
-   },
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // We will display Google, Github, and Email/Password as auth providers.
+  signInOptions: [
+    GoogleAuthProvider.PROVIDER_ID,
+    EmailAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
 };
 
 // Initialize Firebase
@@ -42,11 +41,24 @@ export const db = getFirestore();
 
 // Export FirebaseUI signin screen
 export function SignInScreen() {
-   return (
-      <div>
-         <h1>Sign in to Links for Climate Good</h1>
-         <p>Please sign-in with your Googe account:</p>
-         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-      </div>
-   );
+  return (
+    <div>
+      <h1>Sign in to Links for Climate Good</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+// Function to handle email/password registration
+export async function registerWithEmailAndPassword(email, password) {
+  try {
+    const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
+    // You can do something with the userCredential, like logging the user in
+    console.log('User registered:', userCredential.user);
+    return userCredential.user;
+  } catch (error) {
+    console.error('Error registering user:', error);
+    throw error;
+  }
 }
